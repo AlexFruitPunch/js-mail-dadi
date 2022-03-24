bc724a289c13be979545db7771c615d16831cb04
alexpagani <alexpaganibr@gmail.com>
2022-03-24 15:34:58 +0100

    aggiunta dei controlli sulle email

---
+++ posta/main-posta.js
@@ -0,0 +1,36 @@
+let mail = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
+
+//inserimento email
+alert("Benvenuto sulla nostra pagina ;) ");
+let Utente = prompt("inserisci la tua email per accedere");
+
+Utente = Utente.toLowerCase();
+
+//controllo della email inserita 
+for( let i = 0; i < mail.length; i++){
+    if(mail[i] == Utente){
+        alert("Benvenuto nella nostra pagina"); 
+    }
+}
+
+for( let i = 0; i < mail.length; i++){
+    if (mail[i] != Utente && i == mail.length - 1){
+        alert("non sei registrato ma ti aggiungiamo noi se vuoi");
+
+        //Chiedo all'utente se vuole veramente registrarsi 
+        let scelta = prompt("Vuoi Registrarti che ti registriamo al nostro sito? (inserisci si o no)");
+        scelta = scelta.toLowerCase();
+        if(scelta == "si" || scelta == "no"){ 
+            mail.push(Utente);
+            console.log(mail[mail.length - 1]);
+            alert("grazie per esserti registrato e benvenuto nella nostra pagina");
+        } else {
+            do {
+            alert("devi inserire solo si o no !!! ");
+            scelta = prompt("Vuoi registrarti al nostro sito ?");
+            scelta = scelta.toLowerCase();
+            }
+            while(scelta == "si" || scelta == "no");
+        }
+    }
+}
