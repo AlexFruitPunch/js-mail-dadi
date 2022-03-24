31b17d9ddcfe6631dc4323a741a28cbdf4023f58
alexpagani <alexpaganibr@gmail.com>
2022-03-24 17:32:22 +0100

    Piccole correzioni ma non risolto bottone

--- Dadi/main-dadi.js
+++ Dadi/main-dadi.js
@@ -5,9 +5,15 @@ alert("il vincitore tra me e te sarà quello con il numero più alto");
 let SceltaPC ;
 let SceltaGiocatore ;
 
+//Lancio del dado del PC
 SceltaPC = Math.floor(Math.random() * 6) + 1;
-SceltaGiocatore = Math.floor(Math.random() * 6) + 1;
+document.getElementById("MioLancio").innerHTML = SceltaPC;
 
+//Lancio del dado del giocatore
+SceltaGiocatore = Math.floor(Math.random() * 6) + 1;
+document.getElementById("TuoLancio").innerHTML = SceltaGiocatore;
+
+//Controllo del risultato
 if(SceltaGiocatore > SceltaPC){
     document.getElementById("winner").innerHTML = "il vincitore sei tu";
 
